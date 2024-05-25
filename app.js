f2bbb463664cfb1138a4db3dfe8ea064304c5eec
safeer <safeer.shahul@htpl.cc>
2024-05-25 15:31:41 +0530

    implemeted route filtering for admin routes

--- app.js
+++ app.js
@@ -8,7 +8,11 @@ const shopRoutes = require('./routes/shop')
 // app.use(bodyParser.urlencoded({ extended: true })); old method
 app.use(express.urlencoded({ extended: true })); 
 
-app.use(adminRoutes);
+app.use('/admin',adminRoutes);
 app.use(shopRoutes)
+
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>Page Not Found</h1>')
+})
 
 app.listen(3000, () => console.log('Server running...'));
