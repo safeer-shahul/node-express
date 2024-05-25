7e2d0c751a542b9d74d60602f877a664d56ff371
safeer <safeer.shahul@htpl.cc>
2024-05-25 15:24:12 +0530

    Used express bodyparser and implemented express routes and created seperate route files to handle user and admin routes

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
-const http = require('http')
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
 
-app.use('/users',(req,res,next)=>{
-    console.log('middleware2')
-    res.send('<h1>User</h1>')
-})
+// app.use(bodyParser.urlencoded({ extended: true })); old method
+app.use(express.urlencoded({ extended: true })); 
 
-app.use('/',(req,res,next)=>{
-    console.log('middleware2')
-    res.send('<h1>Hello from Express</h1>')
-})
+app.use(adminRoutes);
+app.use(shopRoutes)
+
 app.listen(3000, () => console.log('Server running...'));
